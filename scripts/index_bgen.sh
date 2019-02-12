809a929efe450768093c2adc3d780b06da5072e6
Jonathan Marten <jonmarten@gmail.com>
2019-02-12 14:43:55 +0000

    Convert bgenix script for array job.

--- scripts/index_bgen.sh
+++ scripts/index_bgen.sh
@@ -1,5 +1,8 @@
-# Create bgi index file for bgen files
-
+#!/bin/bash
 module load gcc/5.2.0
 module load bgenix/1.0.1
-bgenix -g SOMAS_round_all_SOMAS_round_all_impute_1_interval_filtered2_reworked.bgen -index
+
+# Create bgi index file for bgen files
+cd /home/jm2294/GENETIC_DATA/INTERVAL/bgen
+CHR=$SLURM_ARRAY_TASK_ID
+bgenix -g SOMAS_round_all_SOMAS_round_all_impute_${CHR}_interval_filtered2_reworked.bgen -index 
