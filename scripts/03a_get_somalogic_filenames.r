76c8baaf94102cf988935f398ddebb94c813e2bc
Jonathan Marten <jonmarten@gmail.com>
2019-02-19 15:11:34 +0000

    Fix duplicated protein names by appending soma identifier for proteins with multuple assays.

--- scripts/03a_get_somalogic_filenames.r
+++ scripts/03a_get_somalogic_filenames.r
@@ -5,8 +5,14 @@ lapply(c("data.table", "stringr"), require, character.only = T)
 
 protList <- fread("soma_list.txt", data.table = F, h = F)
 protNames <- str_split_fixed(protList$V1,"_",3)
+protFull <- gsub(".sample","",protNames[,3])
 protNames <- str_split_fixed(protNames[,3], "\\.",2)
 protList$protein <- protNames[,1]
 names(protList)[1] <- "filename"
+protList$fullName <- protFull
+
+# Give duplicated names their full identifier
+dupes <- protList$protein %in% unique(protList$protein[ duplicated(protList$protein)]) 
+protList$protein[dupes] <- protList$fullName[dupes]
 
 write.csv(protList, file = "somalogic_proteins_filenames.csv", quote=F, row.names = F)
