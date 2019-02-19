02898f2ecd62e9bd067d272982895b7a7a3057af
Jonathan Marten <jonmarten@gmail.com>
2019-02-19 14:44:46 +0000

    Add script to extract somalogic file and protein names from ls result.

---
+++ scripts/03a_get_somalogic_filenames.r
@@ -0,0 +1,10 @@
+# Script to get file names of Somalogic proteins in a usable order
+lapply(c("data.table", "stringr"), require, character.only = T)
+
+protList <- fread("soma_list.txt", data.table = F, h = F)
+protNames <- str_split_fixed(protList$V1,"_",3)
+protNames <- str_split_fixed(protNames[,3], "\\.",2)
+protList$protein <- protNames[,1]
+names(protList)[1] <- "filename"
+
+write.csv(protList, file = "somalogic_proteins_filenames.csv", quote=F, row.names = F)
